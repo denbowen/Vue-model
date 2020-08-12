768c4b7790b471d1b8635fdf3c1bc85164d082a9
dbwjay <dbwjay@126.com>
2020-08-12 16:42:37 +0800

    add new project

---
+++ App.vue
@@ -0,0 +1,23 @@
+<template>
+  <router-view/>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {},
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang="less">
+html, body {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+  font-size: 14px;
+}
+</style>
